5bc2c05cfa7905aab29545f709dbdab0d68b3c57
陶金 <>
2019-01-05 15:27:35 +0800

    更新模版

---
+++ App/Home/Controller/StaticController.class.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taojin
+ * Date: 2019/1/5
+ * Time: 下午3:07
+ */
+
+namespace Home\Controller;
+
+
+class StaticController extends HomeCommonController
+{
+
+    // 创狮服务总体介绍
+    public function one() {
+        $this->display();
+    }
+
+    // 关于创狮
+    public function two() {
+        $this->display();
+    }
+
+    // 技术开发专区
+    public function three() {
+        $this->display();
+    }
+
+    // 精品案例封面
+    public function four() {
+        $this->display();
+    }
+
+    // 视频设计专区
+    public function five() {
+        $this->display();
+    }
+
+    // 问答营销
+    public function six() {
+        $this->display();
+    }
+
+    // 新闻营销
+    public function seven() {
+        $this->display();
+    }
+
+    // SEM
+    public function eight() {
+        $this->display();
+    }
+
+    // seo
+    public function nine() {
+        $this->display();
+    }
+
+
+}
